f4ec4d6f7187d1898a97082aaa9d161dbc9edc84
Manabu TERADA <terada@cmscom.jp>
2023-01-27 09:40:14 +0000

    add news page

---
+++ src/pages/news.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SampleCard from "@/compnents/SampleCard";
+
+const News = () => {
+    return (
+        <>
+            <div>News page</div>
+            <SampleCard />
+        </>
+    )
+}
+
+export default News
